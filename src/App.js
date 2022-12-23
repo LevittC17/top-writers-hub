61f962cc2623abfc841f3dca87381aa3e8b60980
LevittC17 <LeewednesdayC17@protonmail.com>
2022-12-23 14:34:54 +0300

    Styling the navbar logo

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 
 import {Footer, Blog, Services, Header, WhatGPT3, Features} from './containers';
 import {CTA, Brand, Navbar} from './components';
+import './App.css';
 
 const App = () => {
   return (
